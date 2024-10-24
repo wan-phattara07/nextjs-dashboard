cd33c3dbb9d633713ccce2df133552a8adccbc97
phattara_07@hotmail.com <phattara_07@hotmail.com>
2024-10-24 19:29:43 +0700

    Learn Finish

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,28 @@
-export default function Page(){
-    return <p>Customers Page</p>
+import CustomersTable from '@/app/ui/customers/table';
+import Search from '@/app/ui/search';
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import { lusitana } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Customer',
+};
+
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string;
+    }>;
+}) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+    const customers = await fetchFilteredCustomers(query);
+    return (
+        <div className="w-full">
+            <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+                Customers
+            </h1>
+            <Search placeholder="Search customers..." />
+            <CustomersTable customers={customers} />
+        </div>
+    );
 }
--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -12,12 +12,7 @@ export default async function CustomersTable({
   customers: FormattedCustomersTable[];
 }) {
   return (
-    <div className="w-full">
-      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
-        Customers
-      </h1>
-      <Search placeholder="Search customers..." />
-      <div className="mt-6 flow-root">
+    <div className="mt-6 flow-root">
         <div className="overflow-x-auto">
           <div className="inline-block min-w-full align-middle">
             <div className="overflow-hidden rounded-md bg-gray-50 p-2 md:pt-0">
@@ -118,6 +113,5 @@ export default async function CustomersTable({
           </div>
         </div>
       </div>
-    </div>
   );
 }
